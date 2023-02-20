92d88b663a0ae11cba60ed35baaf132d98b171ad
Kazuki.M <kazuki0922m@gmail.com>
2023-02-21 00:10:10 +0900

    posts　投稿一覧、投稿画面&コントローラー&ルーティングの作成
    rails g controller posts index new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#top'
   get 'home/top'
+
+  get 'posts/index'
+  get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
