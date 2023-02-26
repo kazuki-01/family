382439966f5856915b92a673e46d3a8c9a9d3fc2
Kazuki.M <kazuki0922m@gmail.com>
2023-02-26 22:32:24 +0900

    deviseでコントローラーを編集できるようにルーティングを記入
    登録確認のルーティングを記入

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, module: "users"
+  
   root 'home#top'
   get 'home/top'
 
+  devise_scope :user do
+    get 'users/check' => 'users/registrations#check'
+  end
+  
   get 'posts/index'
   get 'posts/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
