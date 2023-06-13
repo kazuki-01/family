75f99c0880c08bc527ed82e21ab9905fcded9332
Kazuki.M <kazuki0922m@gmail.com>
2023-06-13 22:26:27 +0900

    classificationカラムの追加（タスクの分類）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_13_113848) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_13_131914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_113848) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "classification", null: false
   end
 
   create_table "users", force: :cascade do |t|
