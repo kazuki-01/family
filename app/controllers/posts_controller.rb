88ae85f790e97ceb455aaaa588ca324a2761eeeb
Kazuki.M <kazuki0922m@gmail.com>
2023-07-18 20:42:32 +0900

    チェックボックスをboolean型に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,22 +11,28 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params)
-  # newページで入力した内容が格納された状態のインスタンスが飛んでくる
-    if post.save
+    wday = Weekly.new(weekly_params)
+    if post.classification == "generally"
+       post.save
       redirect_to posts_path
     else
-      render 'new'
-    end
-
-    wday = Weekly.new(task: post.task, emphasis: post.emphasis, labor: post.labor, classification: post.classification, day_of_week: params[:day_of_week])
-    if wday.classification == "generally"
-    elsif wday.classification == "week"
-      wday.save
-    elsif wday.classification == "everyday"
-      wday.day_of_week = '["1", "2", "3", "4", "5", "6", "0"]'
-      wday.save
-    else
-      Rails.logger.debug('aaaaa')
+      if wday.classification == "generally"
+      elsif wday.classification == "week"
+        wday.save
+        redirect_to posts_path
+      elsif wday.classification == "everyday"
+        wday.monday = true
+        wday.tuesday = true
+        wday. wednesday = true
+        wday.thursday = true
+        wday.friday = true
+        wday.saturday = true
+        wday.sunday = true
+        wday.save
+        redirect_to posts_path
+      else
+        Rails.logger.debug('aaaaa')
+      end
     end
   end
 
@@ -50,6 +56,10 @@ class PostsController < ApplicationController
   private
   def post_params
     params.require(:post).permit(:task, :emphasis, :labor, :classification)
+  end
+
+  def weekly_params
+    params.require(:post).permit(:task, :emphasis, :labor, :classification, :monday, :tuesday, :wednesday, :thursday, :friday, :saturday, :sunday)
   end
 
 end
