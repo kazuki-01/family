41f810ff3f1f64ff8dab1ce1d6194f8b43aa3764
Kazuki.M <kazuki0922m@gmail.com>
2023-07-27 23:04:36 +0900

    viewとcontrollerの作成

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -6,6 +6,10 @@ def new
 end
 
 def create
+  chat = Chat.new(chat_params)
+  chat.user_id = current_user.id
+  chat.save
+  redirect_to posts_path
 end
 
 def edit
@@ -15,6 +19,11 @@ def update
 end
 
 def destroy
+end
+
+private
+def chat_params
+  params.require(:chat).permit(:user_id, :done_id, :message)
 end
 
 end
