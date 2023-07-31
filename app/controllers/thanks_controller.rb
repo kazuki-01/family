15ada48fe9a0e216e4c4b0c2ab86279599bf065a
Kazuki.M <kazuki0922m@gmail.com>
2023-07-31 23:09:35 +0900

    お礼の言葉をデータベースに登録

--- app/controllers/thanks_controller.rb
+++ app/controllers/thanks_controller.rb
@@ -1,9 +1,13 @@
 class ThanksController < ApplicationController
 
   def new
+    @thanks = Thank.all.order(created_at: :desc)
   end
 
   def create
+    thank = Thank.new(thank_params)
+    thank.save
+    redirect_to new_thank_path
   end
 
   def edit
@@ -14,5 +18,10 @@ class ThanksController < ApplicationController
 
   def destroy
   end
-  
+
+  private
+  def thank_params
+    params.require(:thank).permit(:text)
+  end
+
 end
