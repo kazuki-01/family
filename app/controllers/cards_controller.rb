8ee323bd6251047d1e76cb896c7dcd5e7d1d7e96
Kazuki.M <kazuki0922m@gmail.com>
2023-09-27 16:28:08 +0900

    cards_controllerの作成

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,5 +1,6 @@
 class CardsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  skip_before_action :verify_authenticity_token
 
   def index
   end
@@ -8,6 +9,23 @@ class CardsController < ApplicationController
   end
 
   def create
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY'] 
+    customer = Payjp::Customer.create(
+      description: 'test', 
+      card: params[:token_id] 
+    )
+
+    card = Card.new(
+      customer_id: customer.id,
+      token_id: params[:token_id],
+      user_id: current_user.id
+    )
+
+    if card.save
+      redirect_to cards_path
+    else
+      redirect_to new_card_path
+    end 
   end
 
   def edit
@@ -21,7 +39,7 @@ class CardsController < ApplicationController
 
   private
   def set_card
-    @card = Creditcard.where(user_id: current_user.id).first if Creditcard.where(user_id: current_user.id).present?
+    @card = Creds.where(user_id: current_user.id).first if Creds.where(user_id: current_user.id).present?
   end
 end
 
